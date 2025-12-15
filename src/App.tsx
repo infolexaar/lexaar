e7dc97851582aed1cb373fd0c47f03c9151d5e6a
KichZero <kichmg22@gmail.com>
2025-12-15 16:51:49 +0200

    Update Block2 component with responsive design and styling improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero";
+import Block2 from "./components/Block2";
 import MapSection from "./components/MapSection";
 import CookieConsent from "./components/CookieConsent";
 import "./App.css";
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="overflow-x-hidden">
       <Hero />
+      <Block2 />
       <MapSection />
       <CookieConsent />
     </div>
